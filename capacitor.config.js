b3b5acde6e0a78a1747483b5afef94e03186120c
DanielTrujilloTapia <22300119@uttt.edu.mx>
2024-05-04 00:47:11 -0600

    first commit

---
+++ capacitor.config.js
@@ -0,0 +1,7 @@
+const config = {
+  appId: 'io.ionic.starter',
+  appName: 'myApp',
+  webDir: 'dist'
+};
+
+module.exports = config;
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,29 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
+    <div>
+      <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
+    </div>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script setup>
+import { IonPage } from '@ionic/vue';
+import { QrcodeStream } from 'vue-qrcode-reader';
+
+const onDecode = (result) => {
+  console.log('QR code result:', result);
+};
+
+const onInit = (promise) => {
+  promise.then(() => {
+    console.log('Camera is ready');
+  })
+  .catch((error) => {
+    console.error('Could not initialize camera', error);
+  });
+};
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
+<style>
+/* Aquí puedes añadir tus estilos */
 </style>
